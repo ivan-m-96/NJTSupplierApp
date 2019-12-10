57f3ab5c0e8e1166943b6a3d9517e60b2476caa2
ivan-m-96 <ivan.m.96@outlook.com>
2019-12-10 17:10:02 +0100

    Backend init
    Added a Spring initializr template

---
+++ SupplierAppBackend/SupplierApp/src/main/java/njt/supplier/SupplierApp/SupplierAppApplication.java
@@ -0,0 +1,13 @@
+package njt.supplier.SupplierApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierAppApplication.class, args);
+	}
+
+}
---
+++ SupplierAppBackend/SupplierApp/src/test/java/njt/supplier/SupplierApp/SupplierAppApplicationTests.java
@@ -0,0 +1,13 @@
+package njt.supplier.SupplierApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
