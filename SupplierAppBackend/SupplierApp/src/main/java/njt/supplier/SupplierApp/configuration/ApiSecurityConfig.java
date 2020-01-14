b6c9d4869d56726d8c3e2f5787d657a2a6366032
ivan-m-96 <ivan.m.96@outlook.com>
2020-01-14 20:38:33 +0100

    Started configuring SpringSecurity

--- SupplierAppBackend/SupplierApp/src/main/java/njt/supplier/SupplierApp/configuration/ApiSecurityConfig.java
+++ SupplierAppBackend/SupplierApp/src/main/java/njt/supplier/SupplierApp/configuration/ApiSecurityConfig.java
@@ -3,13 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package njt.supplier.configuration;
+package njt.supplier.SupplierApp.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -25,10 +26,11 @@ import org.springframework.stereotype.Component;
 @EnableWebSecurity
 public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    RestAuthenticationEntryPoint restAuthenticationEntryPoint;
-    @Autowired
-    AuthenticationSuccessHandler authenticationSuccessHandler;
+//    @Autowired
+//    RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+//    @Autowired
+//    AuthenticationSuccessHandler authenticationSuccessHandler;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication().withUser("admin").password(encoder().encode("adminPass")).roles("ADMIN").and()
@@ -36,24 +38,16 @@ public class ApiSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-          http
-    .csrf().disable()
-    .exceptionHandling()
-    .authenticationEntryPoint(restAuthenticationEntryPoint)
-    .and()
-    .authorizeRequests()
-    .antMatchers("/api/porudzbenice").permitAll()
-    .and()
-    .formLogin()
-    .successHandler(authenticationSuccessHandler)
-    .failureHandler(new SimpleUrlAuthenticationFailureHandler())
-    .and()
-    .logout();
+    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().antMatchers("/api/**");
+
     }
 
-    
-    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/**").hasRole("ADMIN").and().formLogin().and().logout();
+    }
+
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
